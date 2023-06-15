36904eac4d4ac5b41377d45da76261a02061253f
Flora Adia Aurora <faaa@live.com>
2023-06-15 16:41:52 +0800

    INIT next.js

---
+++ components/HoverDemo.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import Link from "next/link"
+import { Space, Tag } from "@arco-design/web-react"
+import { CalendarDays } from "lucide-react"
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button, buttonVariants } from "@/components/ui/button"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+
+export function HoverCardDemo({ name, url }: { name: string; url: string }) {
+  console.log(buttonVariants({ variant: "outline" }))
+  return (
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={url}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          {name}
+        </Link>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-80">
+        <div className="flex justify-between space-x-4">
+          <Avatar>
+            <AvatarImage src="https://github.com/vercel.png" />
+            <AvatarFallback>VC</AvatarFallback>
+          </Avatar>
+          <div className="space-y-1">
+            <h4 className="text-sm font-semibold">{name}</h4>
+            <Tag>Tag 1</Tag>
+            <p className="text-sm">
+              The React Framework – created and maintained by @vercel.
+            </p>
+            {/* <div className="flex items-center pt-2">
+              <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+              <span className="text-xs text-muted-foreground">
+                Joined December 2021
+              </span>
+            </div> */}
+          </div>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  )
+}
---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Jun Show",
+  description:
+    "俊秀：个人云书签, url: jun.sh",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+    docs: "https://ui.shadcn.com",
+  },
+}
